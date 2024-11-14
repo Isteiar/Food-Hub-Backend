f5099b6866cbce012c15b926e2b2be591968e46e
Isteiar <isteiarahmed8@egmail.com>
2024-11-14 19:00:09 +0600

    User model is created

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,3 +1,34 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
+import { IUser } from "../interfaces/User.Interface";
 
-const userSchema = new Schema()
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, "Username is required "],
+      minlength: [3, "Username must be at least 3 characters long"],
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address",
+      ],
+      required: [true, "Email is required "],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required "],
+      minlength: [4, "Password must be at least 4 characters long"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const UserModel = model<IUser>("User", userSchema);
