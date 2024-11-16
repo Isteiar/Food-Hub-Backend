47ae0456fc3893c937eb4a36b9fd9ccb1b37be3d
Isteiar <isteiarahmed8@egmail.com>
2024-11-16 15:46:11 +0600

    User route and user controller for login user is done

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -1,34 +1,65 @@
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 import { Request, Response } from "express";
 import { UserModel } from "../models/User.model";
+import { sign } from "jsonwebtoken";
 
+///register user
 export const createUser = async (req: Request, res: Response) => {
-    const { username, email, password } = req.body;
-  
-    try {
-      //check if email is already exists or not!
-      const userAlreadyExist = await UserModel.findOne({ email });
-      if (userAlreadyExist) {
-        res.status(400).send({
-          message: "User with this email already exists",
-        });
-        return;
-      }
-  
-      //hashing password
-      const hashPassword = await hash(password, 10);
-  
-      //creating a new user
-      const newUser = await UserModel.create({
-        username,
-        email,
-        password: hashPassword,
-      });
-  
-      res.status(201).send({ message: "New user is created", response: newUser });
-    } catch (err) {
-      res.status(400).send({ message: "Could not create user", error: err });
-    }
-  };
+  const { username, email, password } = req.body;
 
-  
+  try {
+    //check if email is already exists or not!
+    const userAlreadyExist = await UserModel.findOne({ email });
+    if (userAlreadyExist) {
+      res.status(400).send({
+        message: "User with this email already exists",
+      });
+      return;
+    }
+
+    //hashing password
+    const hashPassword = await hash(password, 10);
+
+    //creating a new user
+    const newUser = await UserModel.create({
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    res.status(201).send({ message: "New user is created", response: newUser });
+  } catch (err) {
+    res.status(400).send({ message: "Could not create user", error: err });
+  }
+};
+
+//login user
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    //find the user is exist or not!
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      //if user is not found
+      res.status(401).send({ message: "Invalid email" });
+      return;
+    }
+
+    const validPassword = await compare(password, user.password); //(frontendPassword,dbPassword)
+
+    if (!validPassword) {
+      res.status(401).send({ message: "Invalid password" });
+      return;
+    }
+
+    //generate token
+    const accessToken = sign({ id: user._id }, process.env.SECRET as string);
+
+    res
+      .status(200)
+      .send({ message: "User is logged-in", access_token: accessToken });
+  } catch (err) {
+    res.status(500).send({ message: "User can't login" });
+  }
+};
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createUser } from "../controllers/User.controller";
+import { createUser, login } from "../controllers/User.controller";
 
 const userRouter = Router();    
 
 userRouter.post("/resgister", createUser);
-// userRouter.post("/login", login);
+userRouter.post("/login", login);
 
 
 export default userRouter;
