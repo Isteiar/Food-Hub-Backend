556d77d4ff9d38b86a20b64594457459a5eeb970
Isteiar <isteiarahmed8@egmail.com>
2024-11-17 16:16:59 +0600

    Routes for updating and deleting restaurant info is done

--- src/routes/restaurant.route.ts
+++ src/routes/restaurant.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {
   createRestaurant,
+  deleteRestaurant,
   getAllRestaurants,
   getRestaurantById,
+  updateRestaurant,
 } from "../controllers/Restaurant.controller";
 
 const restaurantRouter = Router();
@@ -10,5 +12,7 @@ const restaurantRouter = Router();
 restaurantRouter.post("/create-restaurant", createRestaurant);
 restaurantRouter.get("/all-restaurants", getAllRestaurants);
 restaurantRouter.get("/restaurant", getRestaurantById);
+restaurantRouter.put("/update-restaurant/:id", updateRestaurant);
+restaurantRouter.put("/delete-restaurant/:id", deleteRestaurant);
 
 export default restaurantRouter;
