66731ec883324ae58615e3449519cbdb0a9c195d
Isteiar <isteiarahmed8@egmail.com>
2024-11-17 16:04:16 +0600

    Controller and routes for getting all restaurants info is done

--- src/controllers/Restaurant.controller.ts
+++ src/controllers/Restaurant.controller.ts
@@ -27,3 +27,22 @@ export const createRestaurant = async (req: Request, res: Response) => {
     res.status(500).send({ message: "Error creating restaurant", error: err });
   }
 };
+
+// Get all restaurants
+export const getAllRestaurants = async (_req: Request, res: Response) => {
+  try {
+    const restaurants = await RestaurantModel.find().populate("ownerId");
+
+    if (!restaurants) {
+      res.status(404).send({ message: "Restaurants are not found" });
+      return;
+    }
+    res
+      .status(200)
+      .send({ message: "Restaurants retrieved successfully", restaurants });
+  } catch (err) {
+    res
+      .status(500)
+      .send({ message: "Error retrieving restaurants", error: err });
+  }
+};
